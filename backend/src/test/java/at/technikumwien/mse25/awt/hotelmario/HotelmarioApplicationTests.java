318a0ee4571a0e4d783a5c9317a741a7c9b62e84
Michael Auß <michael.auss@posteo.at>
2026-05-23 16:01:20 +0200

    Create the spring boot backend project with spring boot initializer, use java 25 and gradle

---
+++ backend/src/test/java/at/technikumwien/mse25/awt/hotelmario/HotelmarioApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikumwien.mse25.awt.hotelmario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelmarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
