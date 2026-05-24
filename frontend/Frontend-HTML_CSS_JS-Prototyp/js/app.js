dc7e6393d55e7f674ea4517e2f2a64c7bd500ac8
Patrick Artner <Jackspaar@outlook.at>
2026-05-24 09:53:07 +0200

    Add frontend prototype: index.html with slideshow , style.css, app.js

---
+++ frontend/Frontend-HTML_CSS_JS-Prototyp/js/app.js
@@ -0,0 +1,55 @@
+function validateBookingForm() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+
+    if(firstName === '' || lastName === '' || email === '') {
+        alert('Bitte alle Felder ausfüllen!');
+        return false;
+    }
+
+    return true;
+}
+
+
+/*Slideshow*/
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+let slideIndex = 0;
+showSlidesTimed();
+
+function showSlidesTimed() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlidesTimed, 10000); // Change image every 10 seconds
+}
